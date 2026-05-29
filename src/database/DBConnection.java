a152249c6e2329cf0de27c3c3fba427f5e48b4f4
Edith Agai <email.02yt@gmail.com>
2026-05-29 11:19:51 +0300

    established DB Connection with hidden variables for security

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database;
+import java.sql.*;
 
-/**
- *
- * @author edith
- */
+
 public class DBConnection {
+    //TODO: Add dotenv-java and remove hardcoded credintials 
+    //using transaction pooler (shared pooler)
+    //Added a config file to avoid hard coding values (kinda like .env but .env for java is hard )
+    // url = jdbc:postgres://host:port/databasename
+    public String url = Config.BD_Url;
+    public String username = Config.DB_Username;
+    public String password =  Config.DB_Password;
+    public Connection con;
     
+    public DBConnection(){
+        //1. Load driver 
+        try{
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Driver loading sucessful");
+        }catch( ClassNotFoundException cnfe){
+            System.out.println("failed to load driver"+ cnfe.getMessage());
+        }
+        //2. establic connection
+        try{
+            con = DriverManager.getConnection(url,username,password);
+            System.out.println("Connection Successful!");
+        }catch(SQLException sqle){
+            System.out.println("Connection Failed!" + sqle);
+        }
+    }
+    
+    public void CloseConnection(){
+        try{
+           if (con != null && !con.isClosed() ){
+               con.close();
+               System.out.println("connection closed successfully!");
+           } 
+        }catch(SQLException sqle){
+            System.out.println("failed to close connection" + sqle);
+        }
+    }
 }
+
--- test/dbtest/TestDBConnection.java
+++ test/dbtest/TestDBConnection.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dbtest;
+import database.DBConnection;
 
-/**
- *
- * @author edith
- */
 public class TestDBConnection {
-    
+    public static void main (String[ ] args ){
+        DBConnection db = new DBConnection();
+        db.CloseConnection();
+    }
 }

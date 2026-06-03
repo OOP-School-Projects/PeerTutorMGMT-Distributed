28303e71b55a32186b553856acb866c76829db8d
Edith Agai <email.02yt@gmail.com>
2026-06-03 10:41:23 +0300

    added more fields to signupGUI made dummy dashboard gui (tutee view)

--- src/gui/MainDashboardGUI.java
+++ src/gui/MainDashboardGUI.java
@@ -2,9 +2,7 @@ package gui;
 
 import static javafx.application.Application.launch;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,22 +10,31 @@ public class MainDashboardGUI extends javafx.application.Application{
     @Override 
     public void start(Stage dashboardStage){
         //nodes
-        Label lb = new Label();
-        lb.setText("Hallo!");
-        TextField tf = new TextField();
-        Button btn = new Button();
+        Label lb1 = new Label();
+        lb1.setText("Welcome" /* + getrole*/ );
+        Label lb2 = new Label();
+        lb2.setText("Available sessions");
+        Label lb3 = new Label();
+        lb3.setText("My requests");
+        Button logout = new Button();
+        logout.setText("Log out");
+        logout.setOnAction(e -> {
+            dashboardStage.close();
+        });
+        
         
         
         //scope
             //Layout Manager 
             VBox root = new VBox();
-            root.getChildren().addAll(lb,tf,btn);
+            root.getChildren().addAll(lb1,lb2,lb3,logout);
             
             //Scene
             Scene scene = new Scene(root,800,600);
      
             
         //Stage Management
+        dashboardStage.setTitle("Main Dashboard");
         dashboardStage.setScene(scene);
         dashboardStage.show();
     }
--- src/gui/SignupGUI.java
+++ src/gui/SignupGUI.java
@@ -14,6 +14,9 @@ public class SignupGUI extends Application {
             Label lb1 = new Label();
             lb1.setText("Name: ");
             TextField nameField = new TextField();
+            Label lb5 = new Label();
+            lb5.setText("Student ID: ");
+            TextField idField = new TextField();
             Label lb2 = new Label();
             lb2.setText("Email");
             TextField emailField = new TextField();
@@ -21,6 +24,10 @@ public class SignupGUI extends Application {
             lb3.setText("Password:");
             PasswordField passwordField = new PasswordField();
             Button signupBtn = new Button();
+            Label lb4 = new Label();
+            lb4.setText("Choose a role");
+            ComboBox roleField = new ComboBox();
+            roleField.getItems().addAll("tutee", "tutor", "admin");
             signupBtn.setText("Sign up");
             signupBtn.setOnAction(e -> {
                 Stage dashboardStage = new Stage();
@@ -32,7 +39,7 @@ public class SignupGUI extends Application {
         //scope
             //Layout manager
             VBox root = new VBox();
-            root.getChildren().addAll(lb1, nameField, lb2, emailField,  lb3, passwordField,  signupBtn);
+            root.getChildren().addAll(lb1, nameField, lb5, idField, lb2, emailField,  lb3, passwordField, lb4, roleField,  signupBtn);
             
             //Scene
             Scene scene = new Scene(root, 500, 400);
